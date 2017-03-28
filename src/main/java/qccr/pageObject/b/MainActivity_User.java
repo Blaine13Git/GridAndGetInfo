15dc35867f4e839c919841b52e9937448a7847ad
冯昌 <fengchang@qccr.com>
2017-03-28 15:17:55 +0800

    del 关键字

--- src/main/java/qccr/pageObject/b/MainActivity_User.java
+++
@@ -1,7 +0,0 @@
-package qccr.pageObject.b;
-
-public class MainActivity_User {
-	
-	public static final String ORDER_ALL = "我的采购订单";
-
-}
--- src/main/java/qccr/pageObject/b/VcodeHistoryActivity.java
+++
@@ -1,7 +0,0 @@
-package qccr.pageObject.b;
-
-public class VcodeHistoryActivity {
-	
-	public static final String ALL = "com.twl.qichechaoren_business:id/rb_alltime";
-
-}
